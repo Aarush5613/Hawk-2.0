a5a847c8b8b3088fbe066ebe29487da86d77d564
Aarush5613 <116178513+Aarush5613@users.noreply.github.com>
2026-04-27 15:43:41 +0530

    Update MainActivity.kt

--- app/src/main/java/com/hawk/launcher/MainActivity.kt
+++ app/src/main/java/com/hawk/launcher/MainActivity.kt
@@ -1,26 +1,25 @@
 package com.hawk.launcher
 
-import android.app.ActivityManager
-import android.content.Context
-import android.content.Intent
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.delay
+import android.graphics.Color
+import android.view.Gravity
+import android.widget.TextView
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        val textView = TextView(this)
+        textView.text = "HAWK OS\nSYSTEM READY\n\nWelcome to the Matrix."
+        textView.setTextColor(Color.GREEN)
+        textView.setBackgroundColor(Color.BLACK)
+        textView.textSize = 32f
+        textView.gravity = Gravity.CENTER
+        
+        setContentView(textView)
+    }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
